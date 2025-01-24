2c52b70cc2eb9310cd12c9179e0ac52fa1bf8b4d
Wanpachok Chanchaem <wanpachok.c@mapboss.co.th>
2025-01-24 16:04:48 +0700

    Update All Featrue Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,37 @@
-import { UserJwtPayload } from '@interface/auth.type'
-import { ResponseDto } from '@interface/config/app.config'
+import { UserJwtPayload, UserMeta } from '@interface/auth.type'
+import { ResponseDto, StatustoOut } from '@interface/config/app.config'
 import { errorResponse } from '@interface/config/error.config'
-import { LoginDtoIn, RefreshTokenDtoIn } from '@interface/dto/auth/auth.dto-in'
+import {
+	ChangePasswordProfileDtoIn,
+	ForgetPasswordDtoIn,
+	LoginDtoIn,
+	RefreshTokenDtoIn,
+	ResetPasswordForgotPasswordDtoIn,
+} from '@interface/dto/auth/auth.dto-in'
 import { LoginDtoOut, RefreshTokenDtoOut } from '@interface/dto/auth/auth.dto-out'
 import { UsersEntity } from '@interface/entities'
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
+import { Body, Controller, Post, UnauthorizedException, BadRequestException, UseGuards, Put } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm'
 import * as bcrypt from 'bcryptjs'
 import * as jwt from 'jsonwebtoken'
-import { hashPassword } from 'src/core/utils'
-import { Repository } from 'typeorm'
+import { generateTokenHex, hashPassword, hash } from 'src/core/utils'
+import { EntityManager, Repository } from 'typeorm'
+import { MailService } from 'src/core/mail.service'
+import { AuthGuard } from 'src/core/auth.guard'
+import { User } from 'src/core/user.decorator'
 @Controller('auth')
 export class AuthController {
 	private readonly accessTokenExpried = '1h'
 	private readonly refreshTokenExpried = '1.5h'
 
 	constructor(
+		private readonly mailService: MailService,
+		private readonly config: ConfigService,
+
+		@InjectEntityManager()
+		private readonly entityManager: EntityManager,
+
 		@InjectRepository(UsersEntity)
 		private readonly userEntity: Repository<UsersEntity>,
 	) {}
@@ -29,7 +45,7 @@ export class AuthController {
 	@Post('/login')
 	async login(@Body() body: LoginDtoIn): Promise<ResponseDto<LoginDtoOut>> {
 		const { email, password } = body
-		const user = await this.userEntity.findOne({ where: { email } })
+		const user = await this.userEntity.findOne({ where: { email, isDeleted: false } })
 		if (!user) {
 			throw new UnauthorizedException(errorResponse.INCORRECT_CREDENTIALS)
 		}
@@ -77,5 +93,102 @@ export class AuthController {
 			if (error.name === 'TokenExpiredError') throw new UnauthorizedException(errorResponse.EXPIRED_TOKEN)
 			throw error
 		}
+	}
+
+	@Post('/forget-password')
+	async forgotPassword(@Body() payload: ForgetPasswordDtoIn): Promise<ResponseDto<StatustoOut>> {
+		const email = payload.email
+
+		const user = await this.userEntity.findOne({ where: { email, isDeleted: false } })
+		if (!user) throw new BadRequestException(errorResponse.USER_NOT_FOUND)
+
+		// read config
+		const RESET_PASSWORD_TIMEOUT = this.config.get<number>('RESET_PASSWORD_TIMEOUT')
+		const RESET_PASSWORD_FRONTEND_URL = this.config.get<number>('RESET_PASSWORD_FRONTEND_URL')
+
+		// genereate token and link
+		const resetToken = generateTokenHex(16)
+		const resetLink = `${RESET_PASSWORD_FRONTEND_URL}${resetToken}`
+
+		const now = new Date()
+
+		// set to user row
+		user.resetPasswordExpire = new Date(now.getTime() + 60 * 60 * 1000 * RESET_PASSWORD_TIMEOUT)
+		user.resetPasswordToken = resetToken
+		await this.userEntity.save(user)
+
+		// send mail
+		await this.mailService.sendResetPassword(user.email, user.firstName, resetLink, RESET_PASSWORD_TIMEOUT)
+
+		return new ResponseDto({ data: { success: true } })
+	}
+
+	@Put('/reset-password')
+	async resetPassword(@Body() payload: ResetPasswordForgotPasswordDtoIn): Promise<ResponseDto<StatustoOut>> {
+		const resetPasswordToken = payload.token
+
+		await this.entityManager.transaction(async (transactionalEntityManager) => {
+			const user = await transactionalEntityManager.findOneBy(UsersEntity, {
+				resetPasswordToken,
+				isDeleted: false,
+			})
+			// validate
+			if (!user) throw new BadRequestException(errorResponse.INVALID_TOKEN)
+
+			const now = new Date()
+			if (now > user.resetPasswordExpire) throw new BadRequestException(errorResponse.EXPIRED_TOKEN)
+
+			// set user row
+			user.password = await hash(payload.newPassword)
+			user.resetPasswordToken = null
+			user.resetPasswordExpire = null
+			user.updatedBy = user.userId
+			user.updatedAt = new Date()
+			await transactionalEntityManager.save(user)
+
+			// create log
+			// const newLog = new LogUserEntity()
+			// newLog.operatedAccount = user.email
+			// newLog.operatedBy = { id: user.id }
+			// newLog.operatedDt = new Date()
+			// newLog.type = { id: LutLogUserType.resetPassword }
+			// await transactionalEntityManager.save(newLog)
+		})
+
+		return new ResponseDto({ data: { success: true } })
+	}
+
+	@Put('/change-password')
+	@UseGuards(AuthGuard)
+	async changePassword(
+		@User() user: UserMeta,
+		@Body() putData: ChangePasswordProfileDtoIn,
+	): Promise<ResponseDto<StatustoOut>> {
+		const id = user.id
+		// start transcation
+		await this.entityManager.transaction(async (transactionalEntityManager) => {
+			const userRow = await transactionalEntityManager.findOneBy(UsersEntity, { userId: id })
+
+			const IsValidOldPassword = await bcrypt.compare(putData.oldPassword, userRow.password)
+
+			if (!IsValidOldPassword) throw new BadRequestException('Invalid old password')
+
+			userRow.password = await hashPassword(putData.newPassword)
+			userRow.updatedAt = new Date()
+			userRow.updatedBy = id
+			// update user
+			await transactionalEntityManager.save(userRow)
+
+			// insert log
+			// const newLog = new LogUserEntity()
+			// newLog.operatedDt = new Date()
+			// newLog.operatedBy = { id }
+			// newLog.type = { id: LutLogUserType.changePassword }
+			// newLog.operatedAccount = userRow.email
+
+			// await transactionalEntityManager.save(newLog)
+		})
+
+		return new ResponseDto({ data: { success: true } })
 	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,30 @@ import { AuthController } from './auth.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { TypeOrmConfigService } from '@libs/typeorm/typeorm.service'
 import { UsersEntity } from '@interface/entities'
+import { MailerModule } from '@nestjs-modules/mailer'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MailService } from 'src/core/mail.service'
 @Module({
-	imports: [TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }), TypeOrmModule.forFeature([UsersEntity])],
+	imports: [
+		TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+		TypeOrmModule.forFeature([UsersEntity]),
+		MailerModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				transport: {
+					host: configService.get<string>('MAIL_HOST'),
+					port: configService.get<number>('MAIL_PORT'),
+					secure: true,
+					auth: {
+						user: configService.get<string>('MAIL_USER'),
+						pass: configService.get<string>('MAIL_PASS'),
+					},
+				},
+			}),
+		}),
+	],
 	controllers: [AuthController],
+	providers: [MailService],
 })
 export class AuthModule {}
