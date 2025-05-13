e210ae42beb1637140a363ec916b178e7fd82343
Tua'D <tua.developer@gmail.com>
2025-05-13 07:58:24 +0700

    update env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,7 +2,10 @@ declare namespace NodeJS {
 	interface ProcessEnv {
 		BASE_PATH: string
 		PORT: string
-		APP_URL: string
+		APP_FE_URL: string
+		APP_BE_URL: string
+		RESET_PASSWORD_TIMEOUT: string
+		PROXY_TILE_URL: string
 
 		JWT_SECRET: string
 		JWT_SECRET_REFRESH: string
@@ -23,11 +26,5 @@ declare namespace NodeJS {
 		MAIL_PORT: string
 		MAIL_USER: string
 		MAIL_PASS: string
-		FRONTEND_LOGIN_URL: string
-
-		RESET_PASSWORD_TIMEOUT: string
-		RESET_PASSWORD_FRONTEND_URL: string
-
-		PROXY_TILE_URL: string
 	}
 }
