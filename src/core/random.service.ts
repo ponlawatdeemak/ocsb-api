5cdc32891dbd51aa66457859f2dac32512516bf6
Ponlawat Deemak <ponlawat.deemak@gmail.comtGwn>
2025-05-19 19:23:25 +0700

    fix: delete not use function for security

--- src/core/random.service.ts
+++ src/core/random.service.ts
@@ -3,11 +3,6 @@ import * as crypto from 'crypto'
 
 @Injectable()
 export class RandomService {
-	generateSixDigitString(): string {
-		const randomNumber = Math.floor(Math.random() * 1000000)
-		return randomNumber.toString().padStart(6, '0')
-	}
-
 	generatePassword(): string {
 		const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?'
 		const randomBytes = crypto.randomBytes(12)
